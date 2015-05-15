296a0cdd21aba83680fd54e72f163c6193efb86c
fitchs <fitchs@localhost.localdomain>
2015-05-15 13:06:09 +0200

    ok

--- cd.c
+++ cd.c
@@ -2,18 +2,60 @@
 
 int     b_cd(const char **cmd)
 {
-    ft_putstr("cd\n");
+	char	*path[2];
+	char	buf[256];
+
+	chdir(cmd[0]);
+	path[1] = getcwd(buf, 256);
+	path[0] = ft_strdup("PWD");
+	b_setenv((const char**)path);
+	free(path[0]);
+	return (0);
 }
 
 int     b_env(const char **cmd)
 {
-    ft_putstr("env\n");
+	extern char	**environ;
+	int			i;
+
+	i = 0;
+	while (environ[i + 1])
+	{
+		ft_putstr(environ[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
 }
+
 int     b_setenv(const char **cmd)
 {
-    ft_putstr("setenv\n");
+	char	*tmp;
+	extern char	**environ;
+	int			i;
+	char	*buf;
+
+	i = 0;
+	if (!(tmp = ft_getenv(cmd[0])))
+	{
+		while (environ[i])
+			i++;
+		buf = (char*)malloc(ft_strlen(cmd[0]) + ft_strlen(cmd[1]) + 1);
+		ft_strcpy(buf, cmd[0]);
+		ft_strcat(buf, "=");
+		ft_strcat(buf, cmd[1]);
+		environ[i] = buf;
+	}
+	else
+	{
+		ft_memset(tmp, '\0', ft_strlen(tmp));
+		ft_strcpy(tmp, cmd[1]);
+	}
+	return (0);
 }
+
 int     b_unsetenv(const char **cmd)
 {
     ft_putstr("unsetenv\n");
+	return (0);
 }
--- read.c
+++ read.c
@@ -6,7 +6,7 @@
 /*   By: nchampot <nchampot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/26 14:47:31 by nchampot          #+#    #+#             */
-/*   Updated: 2015/03/26 15:09:49 by nchampot         ###   ########.fr       */
+/*   Updated: 2015/04/02 16:10:08 by nchampot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	**read_cmd(void)
 	char	**tab;
 
     ft_putstr(ft_getenv("PWD"));
-    ft_putstr(" $> ");
+    ft_putstr(" 3===D ");
 	if (get_next_line(0, &buf) == 0)
         return (NULL);
 	tab = ft_strsplit(buf, ' ');
